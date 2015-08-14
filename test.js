7550932adcafe51a886aa6f00fe9e58e300f5430
vics <vics@ebay.com>
2015-08-14 12:47:23 -0700

    comments

--- test.js
+++ test.js
@@ -2,11 +2,18 @@ var lib = require('image-to-text');
 var utils = require('./utils');
 var file = { name: 'iphone.png'};
 
+//check if the API key still works fine.
 lib.getKeywordsForImage(file).then(function(keywords){
     console.log('Passed :)');
     console.log(keywords);
 }).fail(function(data){
+    //if failure, drop a mail to vics
     console.log('Failed!!');
     console.log(data);
-    utils.sendMail('Image To Text Test FAILING!!', data);
+    utils.sendMail('Image To Text Test FAILING!!', data).then(function(){
+        process.exit(1);
+    }).fail(function(){
+        console.log('Failed to send mail');
+        process.exit(1);
+    });
 });
